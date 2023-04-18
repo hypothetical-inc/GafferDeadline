29aa0ed2f9bd432d43b5988fb645781069726acc
Eric Mehl <eric@thinkhypothetical.com>
2023-04-18 19:43:41 -0400

    DeadlineTask : Submit arbitrary Deadline plugins

--- startup/gui/menus.py
+++ startup/gui/menus.py
@@ -1,1 +1,9 @@
+import GafferUI
+
+import GafferDeadline
 import GafferDeadlineUI
+
+
+nodeMenu = GafferUI.NodeMenu.acquire(application)
+
+nodeMenu.append("/Deadline/DeadlineTask", GafferDeadline.DeadlineTask, searchText="DeadlineTask")
